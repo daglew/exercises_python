c7c5cd13bb193ffcb6a4fc758b7d4fef313d8e9a
daglew <daga122611@gmail.com>
2024-01-13 22:45:57 +0100

    przypomnienie

---
+++ print_wprowadzenie/cwiczenie.py
@@ -0,0 +1,12 @@
+"""
+Ćwiczenie 1
+Do zmiennej o nazwie sentence przypisz podane zdanie:
+'I love Python!'
+Następnie korzystając z funkcji print() wydrukuj ten tekst do konsoli.
+Oczekiwany wynik:
+I love Python!
+"""
+
+sentence = "I love Python!"
+print(sentence)
+

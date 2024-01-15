e12382e96b45ec32beb2cdf4a071ae8f94f20961
daglew <daga122611@gmail.com>
2024-01-15 22:27:43 +0100

    przypomnienie_3

---
+++ print_wprowadzenie/cwiczenie_3.py
@@ -0,0 +1,15 @@
+"""
+Ćwiczenie 3
+Podane są poniższe zmienne:
+name = 'John'
+age = 25
+height = 180.5
+Wykorzystując funkcję print() oraz podane zmienne wydrukuj poniższy tekst do konsoli.
+My name is John and I'm 25 years old.
+My height is 180.5 cm.
+
+"""
+name = 'John'
+age = 25
+height = 180.5
+print(f"My name is {name} and I'm {age} years old.\nMy height is {height} cm.")

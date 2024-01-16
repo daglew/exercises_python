a0f9bda0a0d236c0145f857291bcb80912c152f4
daglew <daga122611@gmail.com>
2024-01-16 22:10:46 +0100

    przypomnienie_4

---
+++ print_wprowadzenie/cwiczenie_4.py
@@ -0,0 +1,16 @@
+"""
+Ćwiczenie 4
+Podane są poniższe zmienne:
+name = 'Alice'
+age = 30
+height = 165.5
+Wykorzystując funkcję print() oraz podane zmienne wydrukuj poniższy tekst do konsoli.
+Alice|30|165.5
+"""
+name = 'Alice'
+age = 30
+height = 165.5
+
+print(f"{name}|{age}|{height}")
+
+print(name, age, height, sep='|')

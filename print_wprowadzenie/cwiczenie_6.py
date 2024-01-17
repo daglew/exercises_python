55009df9b0843dbcab7c8ed6b9c0a664dd5b28d5
daglew <daga122611@gmail.com>
2024-01-17 22:28:50 +0100

    przypomnienie_5

---
+++ print_wprowadzenie/cwiczenie_6.py
@@ -0,0 +1,13 @@
+"""
+Ćwiczenie 6
+Stwórz prosty program, który oblicza wartość wyrażenia matematycznego:
+(5 + 3) * 2 / 4. Następnie wykorzystując funkcję print()
+wydrukuj wynik do konsoli w poniższej postaci:
+
+The result is 4.0.
+"""
+
+wynik = (5+3)*2/4
+
+print(f"The result is {wynik}.")
+

397347a0b1d56b4ff32528682c8db5fbbbaec9df
daglew <daga122611@gmail.com>
2024-01-18 22:19:29 +0100

    przypomnienie_6

---
+++ input_wprowadzenie/cwiczenie_1.py
@@ -0,0 +1,15 @@
+
+imie = input("Podaj swoje imie: ")
+
+print("Hej", imie)
+
+print("Hej {} !".format(imie))
+
+jezyk = input("Jakiego języka chcesz się nauczć? ")
+print("Podano język {}".format(jezyk))
+
+imie2 = input("Podaj swoje imie: ")
+wiek = input("Podaj swoj wiek: ")
+
+print("Czesc {}! Masz {} lat.".format(imie2, wiek))
+
---
+++ zmienne_wprowadzenie/cwiczenie_1.py
@@ -0,0 +1,25 @@
+# wszystkie nazwy ktorych nie mozna uzywac
+import keyword
+w = keyword.kwlist
+print(w)
+
+
+a = 5
+b = 10
+
+c = a * b
+print(c)
+
+godziny_przepracowane = 8
+godzinowa_stawka = 10
+
+dzienne_wynagrodzenie = godzinowa_stawka * godziny_przepracowane
+print(dzienne_wynagrodzenie)
+
+
+camelCase = "Python 3.7"
+PascslCase = "Python 3.7"
+snake_case = "Python 3.7"
+kebab_case = "Python 3.7"
+UPPER = "Python"
+

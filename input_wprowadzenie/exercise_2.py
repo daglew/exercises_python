132fbc66b45ea92df153f66c1dbd9216f525f4ab
daglew <daga122611@gmail.com>
2024-01-19 23:40:17 +0100

    input exercise

---
+++ input_wprowadzenie/exercise_2.py
@@ -0,0 +1,19 @@
+"""
+Stwórz program, który wykorzystując operator wycinania z
+podanego ciąg znaków (zmienna text) wyodrębni:
+pierwsze trzy znaki
+ostatnie dwa znaki
+text = 'Python-is-amazing'
+Wynik wydrukuj do konsoli tak jak poniżej.
+Oczekiwany wynik:
+First three characters: Pyt
+Last two characters: ng
+
+"""
+text = 'Python-is-amazing'
+print("First three characters:", text[0:3])
+print("Last two characters:", text[-2:])
+
+
+
+

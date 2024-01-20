ca560cf7b99146731d09db0cb14b5e2b767e304f
daglew <daga122611@gmail.com>
2024-01-20 22:15:56 +0100

    data types

---
+++ data_types/data_types_1.py
@@ -0,0 +1,16 @@
+helo = "Czesc"
+print(dir(helo))
+
+a = 10
+print(dir(a))
+print(type(a))
+print(helo)
+
+b = 2.5
+print(type(b))
+
+c = 5 + 5j
+print(type(c))
+
+print(type(True))
+
---
+++ data_types/exercise_1.py
@@ -0,0 +1,12 @@
+"""
+Ćwiczenie 10
+Utwórz zmienną x, przypisz do niej wartość 5 i wyświetl typ tej zmiennej do konsoli. Następnie zamień wartość tej
+zmiennej na 'five' i ponownie wyświetl typ tej zmiennej do konsoli.
+Oczekiwany wynik:
+<class 'int'>
+<class 'str'>
+"""
+# x = 5
+x = "five"
+print(type(x))
+

c057ac5d994bc820dbea6747050806c33c50028b
daglew <daga122611@gmail.com>
2024-01-29 22:49:46 +0100

    tuple_1

---
+++ tuple_introduction/exercise_4.py
@@ -0,0 +1,19 @@
+"""
+Ćwiczenie 8
+Podana jest poniższa krotka:
+fruits = ('apple', 'banana', 'cherry')
+Stwórz program, który wykorzystuje rozpakowywanie krotki (unpacking), aby przypisać wartości z krotki do zmiennych.
+W odpowiedzi wydrukuj poszczególne zmienne tak jak pokazano poniżej.
+Oczekiwany wynik:
+Tuple: ('apple', 'banana', 'cherry')
+Fruit 1: apple
+Fruit 2: banana
+Fruit 3: cherry
+"""
+fruits = ('apple', 'banana', 'cherry')
+
+print(f"Tuple: {fruits}")
+print(f"Fruit 1: {fruits[0]}")
+print(f"Fruit 2: {fruits[1]}")
+print(f"Fruit 3: {fruits[2]}")
+

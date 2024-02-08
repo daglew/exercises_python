87d9b434b8be8b7dd4181cd24b314a2bf5ad3e6c
daglew <daga122611@gmail.com>
2024-02-08 21:58:33 +0100

    logical_values_1

---
+++ conditional_statements/exercise_2.py
@@ -0,0 +1,20 @@
+"""
+Ćwiczenie 5
+Do zmiennej o nazwie number przypisz wartość 5.
+Następnie wykorzystując instrukcję warunkową sprawdź,
+czy zmienna number jest dodatnia, ujemna czy równa zero.
+W każdym z tych przypadków wydrukuj do konsoli odpowiednio:
+'The number 5 is positive.'
+'The number 5 is negative.'
+'The number 5 is equal to zero.'
+Oczekiwany wynik:
+The number 5 is positive.
+"""
+number = 5
+if number > 0:
+    print("The number 5 is positive.")
+elif number < 0:
+    print("The number 5 is negative.")
+else:
+    print("The number 5 is equal to zero.")
+
---
+++ conditional_statements/exercise_3.py
@@ -0,0 +1,18 @@
+"""
+Ćwiczenie 6
+Do zmiennej o nazwie number przypisz wartość 7.
+Następnie wykorzystując instrukcję warunkową sprawdź,
+czy zmienna number jest liczbą parzystą czy liczbą nieparzystą.
+W każdym z tych dwóch  przypadków wydrukuj do konsoli odpowiednio:
+'The number 7 is even.'
+'The number 7 is odd.'
+Oczekiwany wynik:
+The number 7 is odd.
+"""
+number = 7
+if number % 2 == 0:
+    print('The number 7 is even.')
+else:
+    print('The number 7 is odd.')
+
+

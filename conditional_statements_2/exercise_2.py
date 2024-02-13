64bb969552811d035dbc67250bf7fc0d7fa481d4
daglew <daga122611@gmail.com>
2024-02-13 21:36:32 +0100

    logical_values_2

---
+++ conditional_statements_2/exercise_2.py
@@ -0,0 +1,23 @@
+"""
+Ćwiczenie 12
+Podana jest zmienna tekstowa:
+text = 'sfdvjklncdnskjccbnksjdnckjsdsnckjnsdkjnckjsnkjlcnqdlknwsx'
+Korzystając z instrukcji warunkowej sprawdź czy zmienna ta zawiera znak 'q'.
+Jeżeli tak, wydrukuj do konsoli poniższy tekst:
+The text contains the letter "q".
+w przeciwnym razie:
+The text does not contain the letter "q".
+Oczekiwany wynik:
+The text contains the letter "q".
+"""
+text = 'sfdvjklncdnskjccbnksjdnckjsdsnckjnsdkjnckjsnkjlcnqdlknwsx'
+if "q" in text:
+    print("The text contains the letter 'q'.")
+else:
+    print("The text does not contain the letter 'q'.")
+
+
+
+
+
+

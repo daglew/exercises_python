7920bb28f38ed16ba08733d42d9f8610719e2333
daglew <daga122611@gmail.com>
2024-02-14 22:53:19 +0100

    for_loops_1

---
+++ loops/exercise_1.py
@@ -0,0 +1,30 @@
+"""
+Ćwiczenie 15
+Korzystając z pętli for wydrukuj do konsoli liczby od 0 do 20
+włącznie tak, aby każda liczba znajdowała się w osobnym wierszu
+Oczekiwany wynik:
+0
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10
+11
+12
+13
+14
+15
+16
+17
+18
+19
+20
+"""
+for i in range(0, 21):
+    print(i)
+
